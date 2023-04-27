1eee77ac36c9edebb65f41e5dc8a08b649cd1d53
Davin <davinrim10@gmail.com>
2023-04-27 22:11:31 +0900

    리스트 구현하기

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import '@fontsource/poppins';
 
-function App() {
+export default function App() {
+  const [toDo, setToDo] = useState({ title: '', task: '' });
+  const [toDos, setToDos] = useState([]);
+  const { title, task } = toDo;
+
+  const onChange = (e) => {
+    const { name, value } = e.target;
+    setToDo({ ...toDo, [name]: value });
+  };
+  const onSubmit = (e) => {
+    e.preventDefault();
+    setToDos((prev) => [...prev, toDo]);
+    setToDo({ title: '', task: '' });
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <div className='top'>
+        <h1 className='title'>MY TASKS</h1>
+        <form onSubmit={onSubmit} className='form'>
+          <input
+            className='box'
+            onChange={onChange}
+            name='title'
+            type='text'
+            value={title}
+            placeholder='Add title..'
+          />
+          <input
+            className='box'
+            onChange={onChange}
+            name='task'
+            type='text'
+            value={task}
+            placeholder='Add task..'
+          />
+          <button className='button'>Add</button>
+        </form>
+      </div>
+      <div className='work'>
+        <h2 className='title-work'>Working on it 🔥</h2>
+        {toDos.map((item, index) => (
+          <li key={index}>
+            {item.title} {item.task}
+          </li>
+        ))}
+      </div>
+      <div className='done'>
+        <h2 className='title-work'>Done 🎉</h2>
+      </div>
     </div>
   );
 }
-
-export default App;
