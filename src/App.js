3a0bf5304088230f1fc1ae53115c10f338eec70e
Davin <davinrim10@gmail.com>
2023-04-29 14:22:52 +0900

    삭제 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import { useState } from 'react';
 import './App.css';
 import '@fontsource/poppins';
+import { v4 as uuidv4 } from 'uuid';
 
 export default function App() {
-  const [toDo, setToDo] = useState({ title: '', task: '' });
-  const [toDos, setToDos] = useState([]);
-  const { title, task } = toDo;
+  const [toDo, setToDo] = useState('');
+  const [toDos, setToDos] = useState([
+    {
+      id: uuidv4(),
+      title: 'react',
+      task: 'finish toy project',
+      status: 'isworking',
+    },
+  ]);
+  const { id, title, task, status } = toDo;
 
   const onChange = (e) => {
     const { name, value } = e.target;
@@ -13,9 +21,20 @@ export default function App() {
   };
   const onSubmit = (e) => {
     e.preventDefault();
+    if (toDo.title === '' || toDo.task === '') {
+      return;
+    }
     setToDos((prev) => [...prev, toDo]);
     setToDo({ title: '', task: '' });
   };
+  const handleDelete = (id) => {
+    setToDos(toDos.filter((toDo) => toDo.id !== id));
+  };
+
+  const handleDone = (status) => {
+    setToDo({ status: 'done' });
+  };
+
   return (
     <div className='app'>
       <div className='top'>
@@ -42,14 +61,59 @@ export default function App() {
       </div>
       <div className='work'>
         <h2 className='title-work'>Working on it 🔥</h2>
-        {toDos.map((item, index) => (
-          <li key={index}>
-            {item.title} {item.task}
-          </li>
-        ))}
+        <div>
+          <ul>
+            {toDos.map((toDo) => (
+              <li className='list-row' key={toDo.id}>
+                <div>{toDo.title}</div>
+                <div>{toDo.task}</div>
+                <div className='buttons'>
+                  <button
+                    className='button-done'
+                    onClick={() => handleDone(toDo.status)}
+                  >
+                    done
+                  </button>
+                  <button
+                    className='button-delete'
+                    onClick={() => handleDelete(toDo.id)}
+                  >
+                    delete
+                  </button>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
       <div className='done'>
         <h2 className='title-work'>Done 🎉</h2>
+        <div>
+          <ul>
+            {toDo.status === 'done'
+              ? toDos.filter((toDo) => (
+                  <li className='list-row' key={toDo.id}>
+                    <div>{toDo.title}</div>
+                    <div>{toDo.task}</div>
+                    <div className='buttons'>
+                      <button
+                        className='button-done'
+                        onClick={() => handleDone(toDo.status)}
+                      >
+                        done
+                      </button>
+                      <button
+                        className='button-delete'
+                        onClick={() => handleDelete(toDo.id)}
+                      >
+                        delete
+                      </button>
+                    </div>
+                  </li>
+                ))
+              : ''}
+          </ul>
+        </div>
       </div>
     </div>
   );
